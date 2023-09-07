adca9f4f8b27e86e2f01cd4eb15e69f0cc002426
yusufom <yusufolamilekanmuktar@gmail.com>
2023-09-07 10:01:51 +0100

    fifth

--- base/views.py
+++ base/views.py
@@ -21,7 +21,7 @@ class TaskView(APIView):
         res = {
             "slack_name": slack_name,
             "current_day": datetime.now().strftime('%A'),
-            "utc_time": datetime.now(),
+            "utc_time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
             "track": track,
             "github_file_url": "https://github.com/yusufom/hng_task_one/blob/main/base/views.py",
             "github_repo_url": "https://github.com/yusufom/hng_task_one/",
